f55e9fcd57c1a22a3a6c25af96b61dfa2143272f
lhui <lynn737@gmail.com>
2025-01-06 20:17:49 -0600

    add plant layout improved

--- components/AddPlant.js
+++ components/AddPlant.js
@@ -1,28 +1,39 @@
 import {Button, StyleSheet,View,Text,TextInput} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
+
 export default function AddPlant(){
     const navigation = useNavigation();
     return (
-      <View>
-        {/* <View style={styles.titlebox}/> */}
-        {/* <Button title="Back To Home" onPress = {() => navigation.navigate('Home')}/> */}
-        <Text>Name</Text>
-        <View style={styles.input}>
-          <TextInput />
-        </View>
-        <Text>Species</Text>
-        <View style={styles.input}>
-          <TextInput />
-        </View>
-        <Text>Growing indoors or outdoors?</Text>
-        <View style={{flexDirection:'row',justifyContent:'space-evenly'}}>
-          <View style={{backgroundColor:'green',height:30,width:100}}>
-            <Button color='white' title="indoors"/>
+      <View style={styles.container}>
+        <View style={styles.question}>
+          <Text>Name</Text>
+          <View style={styles.input}>
+            <TextInput />
           </View>
-          <View style={{backgroundColor:'green',height:30,width:100}}>
-            <Button color='white' title="outdoors"/>
+        </View>
+
+        <View style={styles.question}>
+          <Text>Species</Text>
+          <View style={styles.input}>
+            <TextInput />
           </View>
+        </View>
+
+        <View style={styles.question}>
+          <Text>Indoors or outdoors?</Text>
+          <View style={{flexDirection:'row',justifyContent:'space-evenly'}}>
+            <View style={styles.multipleSelect}>
+              <Button color='white' title="indoors"/>
+            </View>
+            <View style={styles.multipleSelect}>
+              <Button color='white' title="outdoors"/>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.submitBtn}>
+            <Button color='white' title="Add me!"/>
         </View>
 
         
@@ -32,11 +43,17 @@ export default function AddPlant(){
 }
 
 const styles = StyleSheet.create({
-  titlebox:{
-    backgroundColor: 'gold',
+  container:{
     width:'100%',
-    height:100,
-    borderRadius: 50
+    height:'100%',
+    flexDirection:'column',
+    justifyContent:'flex-start',
+    alignItems:'center',
+    padding: 30,
+    backgroundColor: 'lightgreen'
+  },
+  question:{
+    padding:15
   },
   input:{
     backgroundColor: 'lightgray',
@@ -44,6 +61,22 @@ const styles = StyleSheet.create({
     height:40,
     width:300,
     flexDirection: 'column',
-    justifyContent:'center'
-  }
+    justifyContent:'center',
+    padding:10
+  },
+  multipleSelect:{
+    backgroundColor:'orange',
+    height:40,
+    width:100,
+    borderRadius: 10
+  },
+  submitBtn:{
+    backgroundColor:'green',
+    height:40,
+    width:100,
+    borderRadius: 10,
+    margin:30,
+  },
+
+
 })
