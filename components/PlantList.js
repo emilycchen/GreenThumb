aa90b5c4ce59d230bf67582670b391409ab1c97b
Emily Chen <echen583@gmail.com>
2025-01-22 16:35:31 -0500

    imported new icons to all plants page

--- components/PlantList.js
+++ components/PlantList.js
@@ -1,25 +1,3 @@
-// export default function PlantList({route}){
-//     const navigation = useNavigation();
-//     const {plants} = route.params;
-//     const h = Dimensions.get('screen').height;
-//     if (plants.length > 5){
-//       h += (plants.length-5)*110
-//     }
-
-//     return (
-//       <View>
-//         <ScrollView contentContainerStyle={{height: h,width:'100%', alignItems:'center'}}>
-//             {
-              
-//                 Array.from({ length: plants.length }).map((_, i) => 
-//                     <PlantListBlock key={i} value={i + 1} plant={plants[i]}/>
-//                   )
-//             }
-//         </ScrollView>
-//       </View>
-  
-//     )
-// }
 import { ScrollView, Text, StyleSheet, View, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import PlantListBlock from './subcomponents/PlantListBlock';
